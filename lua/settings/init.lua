a7e45b2269070a3316618999b4dbff3cf3618ace
Nikosar <randomregistr@gmail.com>
2024-03-25 09:43:25 +0100

    Add git fugitive and powershell on windows

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -28,3 +28,15 @@ vim.keymap.set({"n","v","i"}, "<Up>", "")
 vim.keymap.set({"n","v","i"}, "<Down>", "")
 vim.keymap.set({"n","v","i"}, "<Left>", "")
 vim.keymap.set({"n","v","i"}, "<Right>", "")
+
+system = vim.loop.os_uname().sysname
+print(system)
+if (string.find(system, "Windows")) then
+    vim.cmd[[
+    let &shell = executable('pwsh') ? 'pwsh' : 'powershell'
+    let &shellcmdflag = '-NoLogo -ExecutionPolicy RemoteSigned -noprofile -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';'
+    let &shellredir = '2>&1 | %%{ "$_" } | Out-File %s; exit $LastExitCode'
+    let &shellpipe  = '2>&1 | %%{ "$_" } | Tee-Object %s; exit $LastExitCode'
+    set shellquote= shellxquote=
+    ]]
+end
