e93762b6a28e95bcec7f537dd37fc0dedfb33597
Nikosar <randomregistr@gmail.com>
2024-12-26 18:32:25 +0100

    Modify config

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,16 @@
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1000, 
+--   config = function() 
+--     vim.cmd.colorscheme "catppuccin"
+--   end
+-- }
+return {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  config = function() 
+    vim.cmd.colorscheme "cyberdream"
+  end
+}
